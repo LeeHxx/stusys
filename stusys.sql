07d8d15d067cef7297ac00542d39a291a5c10a05
LeeHxx <spongebob7037@hotmail.com>
2019-11-08 00:11:38 +0800

    Update stusys.sql

--- stusys.sql
+++ stusys.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80013
  File Encoding         : 65001
 
- Date: 07/11/2019 16:59:45
+ Date: 07/11/2019 23:52:35
 */
 
 SET NAMES utf8mb4;
@@ -62,19 +62,57 @@ CREATE TABLE `stu` (
   `jg` varchar(20) DEFAULT NULL,
   `other` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of stu
 -- ----------------------------
 BEGIN;
 INSERT INTO `stu` VALUES (1, '赵威', '男', '16150234', '信息工程学院', '信息管理与信息系统', '信管2班', '122123412334', '1', '2016', '苏州市', NULL);
-INSERT INTO `stu` VALUES (2, '老板', NULL, '16150221', '信息工程学院', '信息管理与信息系统', '信管2班', '12341234123', '山西', '2019', '苏州市', NULL);
-INSERT INTO `stu` VALUES (3, 'boss', '', '16150222', '会计与审计学院', '信息管理与信息系统', '信管2班', '111111111111', '南京', '2017', '无锡市', NULL);
-INSERT INTO `stu` VALUES (18, '茹佳兆', '', '16150221', '会计与审计学院', '信息管理与信息系统', '信管2班', '18812131213', '山西', '2019', '运城市', NULL);
-INSERT INTO `stu` VALUES (20, '王磊', '男', '123', '金融与经济学院', '', '信管2班', '12300000000', '123', '2018', '淮安市', NULL);
-INSERT INTO `stu` VALUES (21, '李浩瑄', NULL, '16150214', '信息工程学院', NULL, '信管2班', '15651006120', '不知道', '2017', '徐州市', '');
-INSERT INTO `stu` VALUES (22, '童禹皓', NULL, '16150222', '信息工程学院', NULL, '信管2班', '18912344321', '南京市', '2016', '淮安市', '');
+INSERT INTO `stu` VALUES (2, '老板', '男', '16150221', '信息工程学院', '信息管理与信息系统', '信管2班', '12341234123', '山西', '2019', '苏州市', NULL);
+INSERT INTO `stu` VALUES (3, 'boss', '女', '16150222', '会计与审计学院', '信息管理与信息系统', '信管2班', '111111111111', '南京', '2017', '无锡市', NULL);
+INSERT INTO `stu` VALUES (18, '茹佳兆', '女', '16150221', '会计与审计学院', '信息管理与信息系统', '信管2班', '18812131213', '山西', '2019', '运城市', NULL);
+INSERT INTO `stu` VALUES (20, '王磊', '男', '123', '信息工程学院', '', '信管1班', '12300000000', '南京市', '2019', '淮安市', NULL);
+INSERT INTO `stu` VALUES (21, '李浩瑄', '男', '16150214', '信息工程学院', NULL, '信管2班', '15651006120', '不知道', '2017', '徐州市', '');
+INSERT INTO `stu` VALUES (22, '童禹皓', '女', '16150222', '信息工程学院', NULL, '信管2班', '18912344321', '南京市', '2016', '淮安市', '');
+INSERT INTO `stu` VALUES (23, '老王', '女', '12345', '信息工程学院', NULL, '信管2班', '12341234123', '家里', '2017', '无锡市', '');
+INSERT INTO `stu` VALUES (24, '测试', '', '111', '金融与经济学院', NULL, '1班', '11122223333', '住址', '2016', '苏州市', '');
+INSERT INTO `stu` VALUES (25, 'test', '男', '1234', '会计与审计学院', NULL, 'class.1', '12345676899', 'test', '2016', '南京市', '');
+INSERT INTO `stu` VALUES (26, 'test1', '', '79797777', '信息工程学院', NULL, 'class.2', '0009087123', 'address', '2019', '淮安市', '');
+INSERT INTO `stu` VALUES (27, 'release', '女', '1212', '信息工程学院', NULL, 'class.12', '12345123523', 'www.qwe', '2017', '无锡市', '');
+INSERT INTO `stu` VALUES (28, 'Cat', '女', '345', '信息工程学院', NULL, '信管', '11111111111', 'cathome', '2019', '徐州市', '');
+INSERT INTO `stu` VALUES (29, '000', '男', '00000', '会计与审计学院', NULL, '信管0班', '00000000000', '000000', '2016', '宿迁市', '');
+INSERT INTO `stu` VALUES (30, 'qe', '女', '3214', '金融与经济学院', NULL, '', '', '', '2015', '常州市', '');
+INSERT INTO `stu` VALUES (31, 'erqw', '女', '4312', '金融与经济学院', NULL, '', '', '', '2015', '宿迁市', '');
+INSERT INTO `stu` VALUES (32, 'twerq', '女', '625', '金融与经济学院', NULL, '', '', '', '2015', '淮安市', '');
+INSERT INTO `stu` VALUES (33, '2345', '女', '14514', '金融与经济学院', NULL, '', '', '', '2015', '泰州市', '');
+INSERT INTO `stu` VALUES (34, 'wertyw', '男', '1355', '会计与审计学院', NULL, '', '', '', '2016', '苏州市', '');
+INSERT INTO `stu` VALUES (35, 'qwer', '男', '3142', '会计与审计学院', NULL, '', '', '', '2015', '常州市', '');
+INSERT INTO `stu` VALUES (36, '321', '男', '1323', '金融与经济学院', NULL, NULL, NULL, NULL, '2015', '南京市', ' ');
+INSERT INTO `stu` VALUES (37, '1231', '男', '21222', '金融与经济学院', NULL, NULL, NULL, NULL, '2015', '南京市', NULL);
+INSERT INTO `stu` VALUES (38, 'qwer', '男', '2121', '金融与经济学院', NULL, NULL, NULL, NULL, '2015', '南京市', NULL);
+INSERT INTO `stu` VALUES (39, 'rqwer', '男', '111', '金融与经济学院', NULL, NULL, NULL, NULL, '2015', '南京市', NULL);
+INSERT INTO `stu` VALUES (40, 'ewqe', '男', '222', '金融与经济学院', NULL, NULL, NULL, NULL, '2016', '南京市', NULL);
+INSERT INTO `stu` VALUES (41, 'q', '女', '333', '金融与经济学院', NULL, NULL, NULL, NULL, '2016', '南京市', NULL);
+INSERT INTO `stu` VALUES (42, 'za', '女', '232', '金融与经济学院', NULL, NULL, NULL, NULL, '2016', '徐州市', NULL);
+INSERT INTO `stu` VALUES (43, 'rr', '女', '3233', '金融与经济学院', NULL, NULL, NULL, NULL, '2017', '徐州市', NULL);
+INSERT INTO `stu` VALUES (44, 'yy', '女', '434', '金融与经济学院', NULL, NULL, NULL, NULL, '2017', '徐州市', NULL);
+INSERT INTO `stu` VALUES (45, 'oo', '女', '321', '金融与经济学院', NULL, NULL, NULL, NULL, '2017', '徐州市', NULL);
+INSERT INTO `stu` VALUES (46, 'bb', '女', '54', '金融与经济学院', NULL, NULL, NULL, NULL, '2018', '苏州市', NULL);
+INSERT INTO `stu` VALUES (47, 'dd', '女', '6433', '金融与经济学院', NULL, NULL, NULL, NULL, '2018', '苏州市', NULL);
+INSERT INTO `stu` VALUES (48, 'bbb', '女', '34', '金融与经济学院', NULL, NULL, NULL, NULL, '2018', '苏州市', NULL);
+INSERT INTO `stu` VALUES (49, 'nn', '女', '342', '金融与经济学院', NULL, NULL, NULL, NULL, '2018', '淮安市', NULL);
+INSERT INTO `stu` VALUES (50, 'mm', '女', '5134', '金融与经济学院', NULL, NULL, NULL, NULL, '2018', '淮安市', NULL);
+INSERT INTO `stu` VALUES (51, 'rte', '男', '123', '信息工程学院', NULL, NULL, NULL, NULL, '2018', '宿迁市', NULL);
+INSERT INTO `stu` VALUES (52, 'hgf', '男', '23', '信息工程学院', NULL, NULL, NULL, NULL, '2018', '宿迁市', NULL);
+INSERT INTO `stu` VALUES (53, 'hhh', '男', '3123', '信息工程学院', NULL, NULL, NULL, NULL, '2018', '宿迁市', NULL);
+INSERT INTO `stu` VALUES (54, 'hh', '男', '333', '信息工程学院', NULL, NULL, NULL, NULL, '2018', '无锡市', NULL);
+INSERT INTO `stu` VALUES (55, '1111', '男', '33', '信息工程学院', NULL, NULL, NULL, NULL, '2018', '无锡市', NULL);
+INSERT INTO `stu` VALUES (56, 'qwe', '男', '3123', '信息工程学院', NULL, NULL, NULL, NULL, '2018', '无锡市', NULL);
+INSERT INTO `stu` VALUES (57, 'qq', '男', '423', '信息工程学院', NULL, NULL, NULL, NULL, '2017', '苏州市', NULL);
+INSERT INTO `stu` VALUES (58, 'ww', '男', '22', '信息工程学院', NULL, NULL, NULL, NULL, '2017', '运城市', NULL);
+INSERT INTO `stu` VALUES (59, 'www', '男', '222', '信息工程学院', NULL, NULL, NULL, NULL, '2017', '泰州市', NULL);
+INSERT INTO `stu` VALUES (60, '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '运城市', NULL);
 COMMIT;
 
 -- ----------------------------
